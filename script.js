00024cdc92d33a5fe708f3a1f930df58d3891840
Hnin Nandar Aung <40712011+michellehna@users.noreply.github.com>
2023-08-22 18:59:15 +0800

    Updated education
    Updated education since graduated

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student","IT Technologist","Developer"],
+        strings: ["Graduate","IT Specialist","Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student","IT Technologist","Developer"],
+        strings: ["Graduate","IT Specialist","Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
